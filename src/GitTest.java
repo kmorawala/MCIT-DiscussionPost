56c7985bbe0f25b58d8189c6c3fe9cacbff826d9
morawala <morawala@seas.upenn.edu>
2019-04-11 20:21:29 +0530

    added GitTest class

---
+++ src/GitTest.java
@@ -0,0 +1,9 @@
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Hello! My Name is Krishna");
+		System.out.println("What is your name? Write some code to print your name to console below");
+		//TODO add your code here
+	}
+
+}
