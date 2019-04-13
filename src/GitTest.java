efdd18f2ad20637ffe5be0d3afc2c52d875407ab
morawala <morawala@seas.upenn.edu>
2019-04-13 20:09:12 +0530

    changed line 7. Added text to line 11.

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,10 +4,12 @@
  */
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello! My Name is Krishna");
+		System.out.println("Hello! How are you? My Name is Krishna");
 		System.out.println("What is your name? Write some code to print your name to console below");
 		//TODO add your code here
 		System.out.println("Hi Krishna, My name is Jinuk Shin and I also go by Luke Shin!");
+		System.out.println("Thanks, Luke! Nice to meet you! lol I changed line 7 to include \"how are you?\"");
+
 	}
 
 }
